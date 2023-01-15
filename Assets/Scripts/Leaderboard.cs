be72e0c082d278d98a2cf8a07996ed400fc70298
Anna Rosca <45275248+annarosca@users.noreply.github.com>
2023-01-15 14:42:06 +0000

    leaderboard can now load dummy data

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class Leaderboard : MonoBehaviour
 {
@@ -23,6 +24,25 @@ public class Leaderboard : MonoBehaviour
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
             entryRectTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            string rankString;
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "th"; break;
+
+                case 1: rankString = "1st"; break;
+                case 2: rankString = "2nd"; break;
+                case 3: rankString = "3rd"; break;
+            }
+
+            int score = Random.Range(1, 99);
+
+            string name = "ABC";
+            entryTransform.Find("PositionText").GetComponent<TextMeshProUGUI>().text = rankString;
+            entryTransform.Find("NameText").GetComponent<TextMeshProUGUI>().text = score.ToString();
+            entryTransform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = name;
         }
     }
 }
